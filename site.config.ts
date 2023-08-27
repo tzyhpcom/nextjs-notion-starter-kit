e9f92e24b419d34a007eabbbf14be7b4bea23fd0
Adam Tang <tzyhpcom@gmail.com>
2023-08-27 14:19:45 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'Vercel-Blog-9337bb9350f24b8b8b76e8b86a2ab43c',
+  rootNotionPageId: '9337bb9350f24b8b8b76e8b86a2ab43c',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
